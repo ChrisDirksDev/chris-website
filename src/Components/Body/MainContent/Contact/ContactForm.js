01d8f0c30befe285f546f72aeab355ea375e2e46
Crexfu <chrisdirks1@gmail.com>
2019-02-12 15:44:03 -0600

    contact form added

--- src/Components/Body/MainContent/Contact/ContactForm.js
+++ src/Components/Body/MainContent/Contact/ContactForm.js
@@ -4,6 +4,7 @@ import emailIcon from './email.png'
 import userIcon from './user.png'
 import messageIcon from './edit.png'
 import TypeText from '../TypeWriterText/TypewriterText'
+import Abutton from '../../../Utils/AButton';
 
 class ContactForm extends Component {
   constructor(){
@@ -14,7 +15,8 @@ class ContactForm extends Component {
       emailDefault: 'Your email',
       email:'',
       messageDefault:'Your message',
-      message:''
+      message:'',
+      emailSent:false
     }
 
   }
@@ -29,79 +31,83 @@ class ContactForm extends Component {
     this.setState({ message: event.target.value });
   }
 
-  async sendEmail() {
+  sendEmail = async () => {
 
-    let nodemailer = require('nodemailer');
-
-    let transporter = nodemailer.createTransport({
-      host: "mail.chrisdirks.com",
-      port: 465,
-      secure: true, // true for 465, false for other ports
-      auth: {
-        user: 'text@chrisdirks.com', // generated ethereal user
-        pass: 'Legacy6354' // generated ethereal password
-      }
-    });
-
-    let mailOptions = { 
-      from: '"Fred Foo 👻" <test@chrisdirks.com>', // sender address
-      to: "chrisdirks1@gmail.com", // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
-      html: "<b>Hello world?</b>" // html body
-    };
-
-    let info = await transporter.sendMail(mailOptions)
-
-    console.log("Message sent: %s", info.messageId);
+    fetch('https://immense-hollows-28003.herokuapp.com/mail', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: this.state.name,
+        email: this.state.email,
+        message: this.state.message
+      })
+    }).then( resp =>{
+      console.log("email request complete", resp.status);
+    })
+    this.setState({emailSent: true});
     // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
   }
 
 
   render() {
-    return (
-      <form className="contactform flex flex-column items-center col-center h-100 mt5">
-        <div className='flex row dynamic-row ms'>
-          <TypeText typedText={['Questions?', 'Just Saying Hi?', 'Work Together?', 'New Project Idea?']}/>
+    if(this.state.emailSent){
+      return(
+        <div className="mt5 ">
+          <span className="email_confirmation sl"> Email Sent!</span>
         </div>
-        <div className='flex row row-center  justify-center form-header'>
-          <span className='block--sl '>Contact</span>
-        </div>
-        {/* 
-        <div className="flex row col-center input">
-          <img src={userIcon}/>
-          <input
-            className='ms text-input' 
-            type="text"
-            value={this.state.name}
-            onChange={this.nameChange}
-            placeholder={this.state.nameDefault}
-          />
-        </div>
-        <div className="flex row col-center input">
-          <img src={emailIcon}/>
-          <input
-            className='ms text-input' 
-            type="text"
-            value={this.state.email}
-            onChange={this.emailChange}s
-            placeholder={this.state.emailDefault}
-          />
-        </div>
-        <div className="flex row flex-start input">
-          <img src={messageIcon}/>
-          <textarea
-            className='ms text-input'
-            value={this.state.message}
-            onChange={this.messageChange}
-            placeholder={this.state.messageDefault}/>
-        </div>
-        <button type="submit" value="Submit" className="w5 h4 mt4 shadow-5"><span className="f-subheadline ">Submit</span></button> 
-        */}
-        <a onClick={this.sendEmail()} href="mailto:chrisdirks.developer@gmail.com"className="f2 white">chrisdirks.developer@gmail.com</a>
-      </form>
-    );
+      )
+    }else{
+      return (
+        <form className="contactform flex flex-column items-center col-center h-100 mt5">
+          <div className='flex row contactform__dynamic-row'>
+            <TypeText typedText={['Questions?', 'Just Saying Hi?', 'Work Together?', 'New Project Idea?']}/>
+          </div>
+          <div className='flex flex-column col-center'>
+          <div className='flex row row-center justify-center contactform__header'>
+            <span className='block--sl '>Contact</span>
+          </div>
+          <div className="flex row col-center input contactform__name-input">
+            <img src={userIcon}/>
+            <input
+              className='ms text-input' 
+              type="text"
+              value={this.state.name}
+              onChange={this.nameChange}
+              placeholder={this.state.nameDefault}
+            />
+          </div>
+          <div className="flex row col-center input  contactform__email-input">
+            <img src={emailIcon}/>
+            <input
+              className='ms text-input' 
+              type="text"
+              value={this.state.email}
+              onChange={this.emailChange}
+              placeholder={this.state.emailDefault}
+            />
+          </div>
+          <div className="flex row flex-start input contactform__message-input">
+            <img src={messageIcon}/>
+            <textarea
+              className='ms text-input'
+              value={this.state.message}
+              onChange={this.messageChange}
+              placeholder={this.state.messageDefault}/>
+          </div>
+          
+  
+          </div>
+          <div className='flex items-center pt4 f2'>
+            <Abutton text='Submit' callback={this.sendEmail}/>
+          </div>
+         
+        </form>
+      );
+
+    }
   }
 
 }
